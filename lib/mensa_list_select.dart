71fd445f684f6c03e2f88c1f88546acbc9aba725
daniel_weissen <dan.weissen@gmail.com>
2019-03-20 17:18:23 +0100

    search canteens working

--- lib/mensa_list_select.dart
+++ lib/mensa_list_select.dart
@@ -18,7 +18,7 @@ class CheckableMensaList extends StatefulWidget {
 class CheckableMensaListState extends State<CheckableMensaList> {
   bool booo = false;
   // TODO once loaded, save canteens to this variable to not so often access the mensa api
-  List<Canteen> listOfCanteens;
+  List<Canteen> listOfCanteens = [];
 
   // final Future<List<Canteen>> mensaList;
   TextEditingController controller = new TextEditingController();
@@ -27,125 +27,77 @@ class CheckableMensaListState extends State<CheckableMensaList> {
 
   @override
   build(BuildContext context) {
-    return FutureBuilder<List<Canteen>>(
-      future: fetchAllCanteens(),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          listOfCanteens = snapshot.data;
-        }
-        return Scaffold(
-          appBar: AppBar(
-            title: Text('Select your Canteens'),
-            actions: <Widget>[
-              IconButton(
-                icon: Icon(Icons.search),
-                tooltip: 'Search',
-                onPressed: !snapshot.hasData
-                    ? null
-                    : () {
-                        showSearch(
-                            context: context,
-                            delegate: CanteenSearch(items: snapshot.data));
-                      },
-              )
-            ],
-          ),
-          /*body: Expanded(
+    // check if canteen list is empty, if it is serve a FutureBuilder while it loads
+    if (listOfCanteens.isEmpty) {
+      print("loaded canteenss");
+      return FutureBuilder<List<Canteen>>(
+        future: fetchAllCanteens(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            listOfCanteens = snapshot.data;
+          }
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Select your Canteens'),
+              actions: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.search),
+                  tooltip: 'Search',
+                  onPressed: !snapshot.hasData
+                      ? null
+                      : () {
+                          showSearch(
+                              context: context,
+                              delegate: CanteenSearch(items: snapshot.data));
+                        },
+                )
+              ],
+            ),
+            /*body: Expanded(
               child: !snapshot.hasData ? Center(child: CircularProgressIndicator()) : createCheckedListView(snapshot.data)
             ),*/
-        );
-      },
+          );
+        },
+      );
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Select your Canteens'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: () {
+              showSearch(
+                  context: context,
+                  delegate: CanteenSearch(items: listOfCanteens));
+            },
+          )
+        ],
+      ),
     );
-/*
-      Scaffold(
-        appBar: AppBar(
-          title: Text('Select your Mensas'),
-          backgroundColor: Colors.black87,
-          actions: <Widget>[
-            FlatButton(
-              child: Text(
-                'confirm',
-                style: TextStyle(color: Colors.white),
-              ),
-              onPressed: () {
-                Navigator.pushNamedAndRemoveUntil(context, '/', (_) => false);
-              },
-            )
-          ],
-        ),
-        body: Column(
-          children: <Widget>[
-            Container(
-              color: Theme.of(context).primaryColor,
-              child: new Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: new Card(
-                  child: new ListTile(
-                    leading: new Icon(Icons.search),
-                    title: new TextField(
-                      controller: controller,
-                      decoration: new InputDecoration(
-                          hintText: 'Search', border: InputBorder.none),
-                      onChanged: onSearchTextChanged,
-                    ),
-                    trailing: new IconButton(icon: new Icon(Icons.cancel), onPressed: () {
-                      controller.clear();
-                      onSearchTextChanged('');
-                    },),
-                  ),
-                ),
-              ),
-            ),
-            Expanded(
-              child: FutureBuilder<ListView>(
-                  future: createCheckedListView(mensaList),
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData) {
-                      return snapshot.data;
-                    } else if (snapshot.hasError) {
-                      if (snapshot.error.toString().contains('RangeError') ||
-                          snapshot.data == null) {
-                        return displayNoMensaFoundMessage(context);
-                      }
-                      return Center(child: Text("Fehlermeldung: ${snapshot.error}"));
-                    }
-                    return Center(child: CircularProgressIndicator());
-                  }),
-            ),
-          ],
-        )
-    );
-    */
   }
 }
 
-class CanteenSearch extends SearchDelegate<Canteen> {
-  List<Canteen> items = [];
-  List<Canteen> suggestion = [];
-  CanteenSearch({@required this.items});
+class ListWidget extends StatefulWidget {
+  final List<Canteen> items;
+  ListWidget({@required this.items});
 
   @override
-  List<Widget> buildActions(BuildContext context) {
-    return [
-      IconButton(
-          icon: Icon(Icons.clear),
-          onPressed: () {
-            query = '';
-          })
-    ];
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return ListWidgetState();
   }
+}
+
+class ListWidgetState extends State<ListWidget> {
+  ListWidgetState();
 
   @override
-  Widget buildSuggestions(BuildContext context) {
-    suggestion = query.isEmpty
-        ? items
-        : items.where((Canteen target) => target.name.contains(query)).toList();
-    if (items.isEmpty) {
-      return displayNoMensaFoundMessage(context);
-    }
+  Widget build(BuildContext context) {
     return Container(
       child: FutureBuilder<ListView>(
-          future: createCheckedListView(suggestion),
+          future: createCheckedListView(widget.items),
           builder: (context, snapshot) {
             if (snapshot.hasData) {
               return snapshot.data;
@@ -160,22 +112,6 @@ class CanteenSearch extends SearchDelegate<Canteen> {
           }),
     );
   }
-
-  @override
-  Widget buildLeading(BuildContext context) {
-    return IconButton(
-      icon: AnimatedIcon(
-        icon: AnimatedIcons.menu_arrow,
-        progress: transitionAnimation,
-      ),
-      onPressed: () {
-        close(context, null);
-      },
-    );
-  }
-
-  @override
-  Widget buildResults(BuildContext context) {}
 
   Future<ListView> createCheckedListView(List<Canteen> canteenList) async {
     final prefs = await SharedPreferences.getInstance();
@@ -209,39 +145,20 @@ class CanteenSearch extends SearchDelegate<Canteen> {
           value:
               checkPrefForMensa(prefs.getStringList('selectedMensas'), canteen),
           onChanged: (bool value) {
-            //setState(() {
-            List<String> selectedMensas =
-                prefs.getStringList('selectedMensas') ?? [];
-            if (value) {
-              selectedMensas.add(canteen);
-              prefs.setStringList('selectedMensas', selectedMensas);
-            } else {
-              selectedMensas.remove(canteen);
-              prefs.setStringList('selectedMensas', selectedMensas);
-            }
-            //});
+            setState(() {
+              List<String> selectedMensas =
+                  prefs.getStringList('selectedMensas') ?? [];
+              if (value) {
+                selectedMensas.add(canteen);
+                prefs.setStringList('selectedMensas', selectedMensas);
+              } else {
+                selectedMensas.remove(canteen);
+                prefs.setStringList('selectedMensas', selectedMensas);
+              }
+            });
           }));
     }
     return mList;
-  }
-
-  Widget displayNoMensaFoundMessage(BuildContext context) {
-    return Center(
-        child: Column(
-      children: <Widget>[
-        Image.asset(
-          'images/sad.png',
-          width: 128.0,
-          height: 128.0,
-          color: Colors.grey[600],
-        ),
-        Text(
-          'Unfortunately no mensas found',
-          style: TextStyle(color: Colors.grey[600]),
-        ),
-      ],
-      mainAxisAlignment: MainAxisAlignment.center,
-    ));
   }
 
   bool checkPrefForMensa(List<String> mensaList, String mensaName) {
@@ -256,5 +173,74 @@ class CanteenSearch extends SearchDelegate<Canteen> {
       }
     }
     return ret;
+  }
+}
+
+Widget displayNoMensaFoundMessage(BuildContext context) {
+  return Center(
+      child: Column(
+    children: <Widget>[
+      Image.asset(
+        'images/sad.png',
+        width: 128.0,
+        height: 128.0,
+        color: Colors.grey[600],
+      ),
+      Text(
+        'Unfortunately no mensas found',
+        style: TextStyle(color: Colors.grey[600]),
+      ),
+    ],
+    mainAxisAlignment: MainAxisAlignment.center,
+  ));
+}
+
+class CanteenSearch extends SearchDelegate<Canteen> {
+  List<Canteen> items = [];
+  List<Canteen> suggestion = [];
+  CanteenSearch({@required this.items});
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+          icon: Icon(Icons.clear),
+          onPressed: () {
+            query = '';
+          })
+    ];
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    suggestion = query.isEmpty
+        ? items
+        : items.where((Canteen target) => target.name.contains(query)).toList();
+    if (items.isEmpty) {
+      return displayNoMensaFoundMessage(context);
+    }
+    return ListWidget(
+      items: suggestion,
+    );
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: AnimatedIcon(
+        icon: AnimatedIcons.menu_arrow,
+        progress: transitionAnimation,
+      ),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return ListWidget(
+      items: suggestion,
+    );
   }
 }

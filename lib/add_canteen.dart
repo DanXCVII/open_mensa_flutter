c6e87a91a12a011aec4309d792d27a18a90926f1
daniel_weissen <dan.weissen@gmail.com>
2019-12-03 15:09:14 +0100

    connected add_canteen ui widget to bloc

--- lib/add_canteen.dart
+++ lib/add_canteen.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:open_mensa_flutter/bloc/canteen_overview/canteen_overview.dart';
+import 'package:open_mensa_flutter/bloc/master/master.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import './fetch_canteens.dart';
 import 'dart:convert';
 import './generated/i18n.dart';
+import 'bloc/master/master_bloc.dart';
 import 'models/canteen.dart';
 
 class AddCanteen extends StatefulWidget {
@@ -21,134 +25,94 @@ class _AddCanteenState extends State<AddCanteen> {
   Widget build(BuildContext context) {
     return Container(
       color: Color(0xff3F3B35),
-      child: FutureBuilder<SharedPreferences>(
-          future: SharedPreferences.getInstance(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              List<String> canteens =
-                  snapshot.data.getStringList('selectedCanteens');
-              bool noCanteenSelected = canteens == null || canteens.length == 0;
-
-              canteens = snapshot.data.getStringList('selectedCanteens');
-
-              return CustomScrollView(slivers: <Widget>[
-                SliverAppBar(
-                  expandedHeight: 200.0,
-                  floating: false,
-                  pinned: true,
-                  flexibleSpace: FlexibleSpaceBar(
-                    background:
-                        Image.asset('images/earth.jpg', fit: BoxFit.cover),
-                    title: Text(S.of(context).selected_canteens),
-                  ),
+      child: BlocBuilder<CanteenOverviewBloc, CanteenOverviewState>(
+        builder: (context, state) {
+          if (state is LoadingCanteenOverviewState) {
+            return Center(child: CircularProgressIndicator());
+          } else if (state is LoadedCanteenOverviewState) {
+            return CustomScrollView(slivers: <Widget>[
+              SliverAppBar(
+                expandedHeight: 200.0,
+                floating: false,
+                pinned: true,
+                flexibleSpace: FlexibleSpaceBar(
+                  background:
+                      Image.asset('images/earth.jpg', fit: BoxFit.cover),
+                  title: Text(S.of(context).selected_canteens),
                 ),
-                SliverList(
-                    delegate: SliverChildListDelegate(noCanteenSelected
-                        ? [noCanteenSelectedText()]
-                        : getCanteenList(snapshot.data, canteens)))
-              ]);
-            } else if (snapshot.hasError) {
-              return Text('add_canteen build Error: ${snapshot.error}');
-            }
-            return (Center(
-              child: CircularProgressIndicator(),
-            ));
-          }),
-    );
-  }
-
-  List<Widget> getCanteenList(SharedPreferences prefs, canteens) {
-    List<Widget> canteenWidgetList = [];
-    for (int i = 0; i < canteens.length; i++) {
-      Canteen canteen = Canteen.fromJson(json.decode(canteens[i]));
-      canteenWidgetList.add(Dismissible(
-        key: Key(canteens[i]),
-        onDismissed: (direction) {
-          setState(() {
-            List<String> tmp = prefs.getStringList('selectedCanteens');
-            tmp.remove(canteens[i]);
-            prefs.setStringList('selectedCanteens', tmp);
-          });
+              ),
+              SliverList(
+                delegate: SliverChildListDelegate(
+                  state.selectedCanteens.isEmpty
+                      ? [noCanteenSelectedText()]
+                      : state.selectedCanteens
+                          .map(
+                            (canteen) => Dismissible(
+                              key: Key(canteen.toString()),
+                              onDismissed: (direction) {
+                                setState(() {
+                                  BlocProvider.of<MasterBloc>(context)
+                                      .add(MDeleteCanteenEvent(canteen));
+                                });
+                              },
+                              child: Card(
+                                child: ListTile(
+                                  title: Padding(
+                                      padding:
+                                          EdgeInsets.symmetric(vertical: 8.0),
+                                      child: Text(canteen.name)),
+                                  subtitle: Padding(
+                                      padding: EdgeInsets.only(bottom: 8.0),
+                                      child: Text(canteen.address)),
+                                  trailing: IconButton(
+                                    icon: Icon(Icons.directions),
+                                    onPressed: () {
+                                      // TODO: Add Directions to Canteen
+                                    },
+                                  ),
+                                ),
+                              ),
+                              background: Container(
+                                color: Colors.red[800],
+                                alignment: Alignment.centerRight,
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: <Widget>[
+                                    Padding(
+                                        padding: EdgeInsets.only(left: 18.0),
+                                        child: Icon(
+                                          Icons.delete,
+                                          color: Colors.white,
+                                        )),
+                                    Padding(
+                                      padding: EdgeInsets.only(right: 18.0),
+                                      child: Icon(
+                                        Icons.delete,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          )
+                          .toList(),
+                ),
+              )
+            ]);
+          }
         },
-        child: Card(
-          child: ListTile(
-            title: Padding(
-                padding: EdgeInsets.symmetric(vertical: 8.0),
-                child: Text(canteen.name)),
-            subtitle: Padding(
-                padding: EdgeInsets.only(bottom: 8.0),
-                child: Text(canteen.address)),
-            trailing: IconButton(
-              icon: Icon(Icons.directions),
-              onPressed: () {
-                // TODO: Add Directions to Canteen
-              },
-            ),
-          ),
-        ),
-        background: Container(
-          color: Colors.red[800],
-          alignment: Alignment.centerRight,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Padding(
-                  padding: EdgeInsets.only(left: 18.0),
-                  child: Icon(
-                    Icons.delete,
-                    color: Colors.white,
-                  )),
-              Padding(
-                  padding: EdgeInsets.only(right: 18.0),
-                  child: Icon(
-                    Icons.delete,
-                    color: Colors.white,
-                  )),
-            ],
-          ),
-        ),
-      ));
-    }
-    return canteenWidgetList;
-  }
-
-  CustomScrollView canteenList(SharedPreferences prefs, canteens) {
-    if (canteens == null || canteens.length == 0) {
-      return noCanteenSelectedText();
-    } else {
-      return CustomScrollView(slivers: <Widget>[
-        SliverAppBar(
-          expandedHeight: 200.0,
-          floating: false,
-          pinned: true,
-          flexibleSpace: FlexibleSpaceBar(
-            background: Image.asset('images/earth.jpg', fit: BoxFit.cover),
-            title: Text(S.of(context).selected_canteens),
-          ),
-        ),
-        SliverList(
-            delegate: SliverChildListDelegate(getCanteenList(prefs, canteens)))
-      ]);
-    }
-  }
-
-  String getCanteenAddress(String fullCanteenInfo) {
-    return fullCanteenInfo.split('&&')[1].split('&&&')[0];
+      ),
+    );
   }
 
   Widget noCanteenSelectedText() {
     return Container(
-        height: MediaQuery.of(context).size.height / 2,
-        child: Center(
-          child: Text(S.of(context).no_canteen_selected),
-        ));
+      height: MediaQuery.of(context).size.height / 2,
+      child: Center(
+        child: Text(S.of(context).no_canteen_selected),
+      ),
+    );
   }
-}
-
-String getCanteenName(String fullCanteenInfo) {
-  return fullCanteenInfo.split('&&')[0].split('&')[1];
-}
-
-String getCanteenId(String fullCanteenInfo) {
-  return fullCanteenInfo.split('&')[0];
 }
